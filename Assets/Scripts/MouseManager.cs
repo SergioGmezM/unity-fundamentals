783f3e6d4cc83dd75ad5db72420c61931c662a1f
TheBender10 <sergiogomez9867@gmail.com>
2021-05-12 10:01:28 +0200

    Add basic player movement through NavMesh

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MouseManager : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class MouseManager : MonoBehaviour
     public Texture2D target; // Cursor for clickable objects like the world
     public Texture2D doorway; // Cursor for doorways
     public Texture2D combat; // Cursor for combat actions
+
+    // Listens to the event and passes a Vector3 as a parameter
+    public EventVector3 onClickEnvironment;
 
     // Update is called once per frame
     void Update()
@@ -31,6 +35,12 @@ public class MouseManager : MonoBehaviour
             {
                 Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
             }
+
+            // Moving the player
+            if (Input.GetMouseButtonDown(1))
+            {
+                onClickEnvironment.Invoke(hit.point);
+            }
         }
         else
         {
@@ -38,3 +48,7 @@ public class MouseManager : MonoBehaviour
         }
     }
 }
+
+// Makes it available in the inspector
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
