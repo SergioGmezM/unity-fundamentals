f22628905178012c008d6ffd10270a8158c97018
TheBender10 <sergiogomez9867@gmail.com>
2021-06-02 09:14:28 +0200

    Camera follows the player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
